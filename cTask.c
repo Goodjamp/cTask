d4df2571e199924126409f37b455971ded831b87
The-new-hope <vuki5@ukr.net>
2020-03-05 11:34:09 +0200

    05.03.20
    05.03.20

--- cTask.c
+++ cTask.c
@@ -14,19 +14,35 @@ Create function for converting number to string. Don't use standard C library
 Please, take into account, that function must process full range INT value
 */
 
-uint8_t *task1_intToStr(int32_t number) 
+uint8_t *task1_intToStr(int32_t number)
 {
-
+    static uint8_t buffer[11];
+    for (int8_t i = 9; i>=0; i--)
+    {
+        buffer[i] = number % 10 + '0';
+        number /= 10;
+    }
+    for (int8_t i = 0; i<=9; i++)
+    {
+        if (buffer[0] == 0 + '0')
+        {
+            for(int8_t q = 0; q<=9; q++)
+            {
+               buffer[q] = buffer[q+1];
+            }
+            buffer[9] = '\0';
+        }
+    }
+    buffer[10] = '\0';
+   return buffer;
 }
 
 void main(char **inArg, int numArg)
 {
     int32_t task1Num1 = 23;
 	int32_t task1Num2 = 1000001;
-	
-    printf("**************TASK 1****************\n");
-	
+
     printf("num1 = %s\n", task1_intToStr(task1Num1));
     printf("num2 = %s\n", task1_intToStr(task1Num2));
-
+    return 0;
 }
